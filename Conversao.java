3793c1e3e8b013f2636374433744ab73870c02c3
Thaylon <ThaylonHenrique586@gmail.com>
2024-11-05 22:00:51 -0300

    first commit

---
+++ Conversao.java
@@ -0,0 +1,10 @@
+public class Conversao {
+
+    public static void main(String[] args) {
+        
+        int num1 = 10000;
+        System.out.println(num1);
+        Integer num2 = num1;
+        System.out.println(num2);
+    }
+}
