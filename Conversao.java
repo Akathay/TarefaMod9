c31c0ee51c80d5fd88a8b24f37c7cef2fa5adf16
Thaylon <ThaylonHenrique586@gmail.com>
2024-11-05 22:02:36 -0300

    Correcao

--- Conversao.java
+++ Conversao.java
@@ -2,7 +2,7 @@ public class Conversao {
 
     public static void main(String[] args) {
         
-        int num1 = 10000;
+        int num1 = 1;
         System.out.println(num1);
         Integer num2 = num1;
         System.out.println(num2);
